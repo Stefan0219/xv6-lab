fcd41844907d800352819ff23c26d1bf546624a1
Stefan Tian <StefanTian02@outlook.com>
2022-09-29 15:51:48 +0800

    nothing happend

--- user/test.c
+++ user/test.c
@@ -2,14 +2,6 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int main(int argc,char *argv[]){
-    char a[2][2];
-    a[0][0] = '1';
-    a[0][1] = '\0';
-    a[1][0] = '2';
-    a[1][1] = '\0';
-    char * A[1];
-    A[0] = a[0];
-    printf("%s\n",A[0]);
+int main(int argc,char *argv[]){    
     return 0;
 } 
